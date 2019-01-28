2dcd1b17aa883e651e3be10ac8dee4a590de3ccd
Martins Mozeiko <martins.mozeiko@gmail.com>
2019-01-27 21:16:11 -0800

    add ffmpeg v4.1

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -eux
+
+mkdir -p /mnt/host/{archive,download}
+
+FFMPEG_VERSION=4.1
+
+FFMPEG_ARGS="                        \
+  --arch=x86                         \
+  --target-os=mingw32                \
+  --cross-prefix=x86_64-w64-mingw32- \
+                                     \
+  --pkg-config=pkg-config            \
+  --pkg-config-flags=--static        \
+  --disable-debug                    \
+  --disable-doc                      \
+  --enable-gpl                       \
+  --enable-version3                  \
+  --enable-nonfree                   \
+"
+
+function get()
+{
+  FILE="/mnt/host/download/${1##*/}"
+  if [ ! -f "${FILE}" ]; then
+    wget -q "$1" -O "${FILE}"
+  fi
+   
+  case "${1##*.}" in
+  gz|tgz)
+    tar --warning=none -xzf "${FILE}"
+    ;;
+  bz2)
+    tar --warning=none -xjf "${FILE}"
+    ;;
+  xz)
+    tar --warning=none -xJf "${FILE}"
+    ;;
+  *)
+    echo unknown archive extension "${1##*.}"
+    exit 1
+    ;;
+  esac
+}
+
+function mkcd()
+{
+  mkdir -p "$1" && cd "$1"
+}
+
+function build_ffmpeg()
+{
+  FFMPEG_VERSION=$1
+
+  get https://www.ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz
+  pushd ffmpeg-${FFMPEG_VERSION}
+
+  mkcd build
+  ../configure ${FFMPEG_ARGS}
+  make -j`nproc`
+
+  zip -9 /mnt/host/archive/ffmpeg-${FFMPEG_VERSION}-`date +%Y%m%d`.zip ffmpeg.exe ffprobe.exe
+
+  popd
+}
+
+mkcd /mnt/ffmpeg
+
+build_ffmpeg "${FFMPEG_VERSION}"
