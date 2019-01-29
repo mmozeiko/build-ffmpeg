31129354e82d8a692ce52065f211d6da0e5f6bc8
Martins Mozeiko <martins.mozeiko@gmail.com>
2019-01-28 23:42:37 -0800

    add sdl2 v2.0.9

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ if [ ! -v CORE_COUNT ]; then
   CORE_COUNT=`nproc`
 fi
 
+SDL2_VERSION=2.0.9
+
 FFMPEG_VERSION=4.1
 
 FFMPEG_ARGS="                        \
@@ -22,6 +24,27 @@ FFMPEG_ARGS="                        \
   --enable-gpl                       \
   --enable-version3                  \
   --enable-nonfree                   \
+"
+
+CONFIGURE_ARGS="            \
+  --prefix=${MINGW}         \
+  --host=x86_64-w64-mingw32 \
+  --disable-shared          \
+  --enable-static           \
+"
+
+CMAKE_ARGS="                                     \
+  -DCMAKE_BUILD_TYPE=Release                     \
+  -DCMAKE_SYSTEM_NAME=Windows                    \
+  -DCMAKE_INSTALL_PREFIX=${MINGW}                \
+  -DCMAKE_FIND_ROOT_PATH=${MINGW}                \
+  -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER      \
+  -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY       \
+  -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY       \
+  -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc      \
+  -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++    \
+  -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres \
+  -DBUILD_SHARED_LIBS=OFF                        \
 "
 
 function get()
@@ -53,10 +76,34 @@ function mkcd()
   mkdir -p "$1" && cd "$1"
 }
 
+function build_sdl2()
+{
+  get https://www.libsdl.org/release/SDL2-${SDL2_VERSION}.tar.gz
+  pushd SDL2-${SDL2_VERSION}
+
+  ./configure ${CONFIGURE_ARGS} \
+    --disable-joystick          \
+    --disable-haptic            \
+    --disable-sensor            \
+    --disable-power             \
+    --disable-filesystem        \
+    --disable-file              \
+    --disable-diskaudio         \
+    --disable-dummyaudio        \
+    --disable-video-dummy       \
+    --disable-video-opengles    \
+    --disable-video-vulkan      \
+    CFLAGS="-DDECLSPEC="
+
+  patch -p0 -i ../../patch/sdl2-no-dynapi.patch
+  make -j${CORE_COUNT}
+  make install
+
+  popd
+}
+
 function build_ffmpeg()
 {
-  FFMPEG_VERSION=$1
-
   get https://www.ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz
   pushd ffmpeg-${FFMPEG_VERSION}
 
@@ -64,11 +111,12 @@ function build_ffmpeg()
   ../configure ${FFMPEG_ARGS}
   make -j${CORE_COUNT}
 
-  zip -9 ../../../archive/ffmpeg-${FFMPEG_VERSION}-`date +%Y%m%d`.zip ffmpeg.exe ffprobe.exe
+  zip -9 ../../../archive/ffmpeg-${FFMPEG_VERSION}-`date +%Y%m%d`.zip ffmpeg.exe ffprobe.exe ffplay.exe
 
   popd
 }
 
 mkcd build
 
-build_ffmpeg "${FFMPEG_VERSION}"
+build_sdl2
+build_ffmpeg
